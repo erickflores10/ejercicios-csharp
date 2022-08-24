e1c9c1e698cc9f84b544d508d8a8bcb077b48d8c
Mythos <developerwebmythos@gmail.com>
2022-08-24 13:57:40 -0500

    Ejercicio 2 de laboratorio

---
+++ semana-2/lab-2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte num_1, num_2;
+
+            Console.Write("Primer numero: ");
+            num_1 = byte.Parse(Console.ReadLine());
+
+            Console.Write("Segundo numero: ");
+            num_2 = byte.Parse(Console.ReadLine());
+
+            if (num_1 > num_2)
+            {
+                Console.WriteLine("El numero " + num_1 + " es mayor que " + num_2);
+            } else
+            {
+                Console.WriteLine("El numero " + num_2 + " es mayor que " + num_1);
+            }
+        }
+    }
+}
